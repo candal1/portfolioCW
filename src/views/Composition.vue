01d141c56ef0961de696bb1e4363b838c49b7f6a
Cole <snwcol16@gmail.com>
2020-06-24 18:34:39 -0400

    comp page changes

--- src/views/Composition.vue
+++ src/views/Composition.vue
@@ -6,17 +6,17 @@
             <span class="text-2xl italic"> <br>by Steve Martin</span><br>
             <span class="text-xl leading-loose">Composers: Chris Wilson, Drew Stockero</span>
         </div>
-        <div class="inline flex">
-            <img src="../assets/comp_1.jpg" alt="picasso stage view">
+        <div class="md:inline-flex">
+            <img class="max-w-md" src="../assets/comp_1.jpg" alt="picasso stage view">
             <p class="max-w-md px-10 py-5">My role for this production of Picasso at the Lapin Agile was Co-Composer. This production was designed in a large part by the students. Here are a couple of songs I composed for this play.</p>
         </div>
-        <div class="inline flex">
-            <img src="../assets/comp_2.jpg" alt="picasso stage view">
-            <p> Drew and I were the two composers for this production. The two of us decided before the first production meeting to split up the work between us. To ensure that the overall composition would make sense, we sent our rough drafts back and forth as to have a clear communication of ideas throughout. Having a two week timeline to compose, this was the most efficient approach.</p>
+        <div class="md:inline-flex">
+            <img class="max-w-md" src="../assets/comp_2.jpg" alt="picasso stage view">
+            <p class="max-w-sm px-10 py-5"> Drew and I were the two composers for this production. The two of us decided before the first production meeting to split up the work between us. To ensure that the overall composition would make sense, we sent our rough drafts back and forth as to have a clear communication of ideas throughout. Having a two week timeline to compose, this was the most efficient approach.</p>
         </div>
-        <div class="inline flex">
-            <img src="../assets/comp_3.jpg" alt="picasso stage view">
-            <p> Tech week is when we made the most critical changes to make it fit with the actions and timings of the production. We worked closely with the Sound Designer to ensure that the music and sound effects meshed well to create a consistent sound. We got feedback from the director </p>
+        <div class="md:inline-flex">
+            <img class="max-w-md" src="../assets/comp_3.jpg" alt="picasso stage view">
+            <p class="max-w-sm px-10 py-5"> Tech week is when we made the most critical changes to make it fit with the actions and timings of the production. We worked closely with the Sound Designer to ensure that the music and sound effects meshed well to create a consistent sound. We got feedback from the director </p>
         </div>
     </div>
 </template>
