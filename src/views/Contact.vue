9ecb57b75736d2abc6e2e3f51efc87c950c5626a
candall <cekendal@mtu.edu>
2020-06-27 16:29:34 -0400

    contact and experience pages

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
+    <div class="bg-blue-900 border-b">
+        <div class="flex py-32 justify-center">
+            <img class="max-w-lg w-full h-auto" src="..\assets\contact_chris.jpg" alt="Chris sitting at sound bourd">
+            <form class="px-5 text-white max-w-xl w-full">
+                <h1 class="text-4xl font-bold tracking-widest">Contact</h1>
+                <p class="max-w-xs py-12">Let me know if there is anything I can do for you. I'll get back with you within 1 business day.</p>
+                <input class="bg-blue-900 border-4 border-white appearance-none py-2 px-3 my-2 w-full" type="text" placeholder="Name *">
+                <input class="bg-blue-900 border-4 border-white appearance-none py-2 px-3 my-2 w-full" type="text" placeholder="Email *">
+                <input class="bg-blue-900 border-4 border-white appearance-none py-2 px-3 my-2 w-full" type="text" placeholder="Subject">
+                <textarea class="bg-blue-900 border-4 border-white appearance-none w-full my-2 p-2" cols="55" rows="6" placeholder="Message"> </textarea>
+                <button class="inine-flex bg-orange-100 p-5 text-black h-10 w-32 items-">Submit</button>
+            </form>
+
+        </div>
 
     </div>
 </template>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="bg-orange-100">
-        <div class="flex flex-col py-20">
-            
-        </div>
+    <div class="bg-orange-100 flex flex-col items-center">
+        <div class="grid py-20 items-center max-w-xl">
 
+           <img class="max-w-lg h-auto m-auto pt-10" src="..\assets\experience\mix factory one.jpg" alt="mix one studio"> 
+           <h2  class="font-bold text-3xl uppercase pt-3 text-center trackng-tighter">Internship</h2>
+           <h2  class="italic text-2xl pb-2">Mix Factory One Studio</h2>
+           <p   class="text-center text-sm pb-10">As an intern, I would assist the recording engineer with anything that they or the client needed. I was given the task of prepping the studios before each session to make sure all settings were correct for the equipment they had. When producers and other clients would come in, they would change routing and other settings in Pro Tools which would cause for endless troubleshooting during sessions if I wasn't there to reset the settings. Also, one of the recording engineers taught me how to run a rap/hip hop session from start to finish. </p>
+
+           <img class="max-w-lg h-auto m-auto pt-10" src="..\assets\experience\26242736452_7c01c61709_k.jpg" alt="Winter's Tale stage scene"> 
+           <h2  class="font-bold text-3xl uppercase pt-3 text-center tracking-tighter">RF Crew</h2>
+           <h2  class="italic text-3xl pb-2">Winter's Tale <span class="text-lg">by William Shakespeare</span></h2>
+           <p   class="text-center text-sm pb-10">For this production, I chose to be a part of the RF Crew. One of the main responsibilities we had was to mic up the actors before each show. We would have to prep the actor with alcohol wipes before taping on the lavalier mic. Throughout the show we would monitor the wireless packs to make sure they had enough battery and signal.</p>
+
+           <img class="max-w-lg h-auto m-auto pt-10" src="..\assets\experience\31097085625_60132efd33_k.jpg" alt="Silent Sky stage"> 
+           <h2  class="font-bold text-3xl uppercase pt-3 text-center tracking-tighter">Co-Properties Master & Run Crew</h2>
+           <h2  class="italic text-3xl pb-2 ">Silent Sky <span class="text-lg">by Lauren Gunderson</span></h2>
+           <p   class="text-center text-sm">For this production, I was Co-Properties Master and Run Crew. My main responsibilities for Co-Properties Master were to create star plates, boxes to store the plates, and a period-correct hearing aid. During the show, Henrietta Leavitt would drop a star plate which is scripted to shatter into pieces. To achieve this effect, I pre-broke a plexiglass star plate and would sparsely glue it together every day before the show to where it would break upon hitting the ground. I have a background in carpentry so I fit this position nicely.</p>
+        </div>
     </div>
 </template>
 
